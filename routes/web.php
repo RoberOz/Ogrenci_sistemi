9ecea0ba00cc1207c278baaf709d8f8d54a2e4e5
RoberOZ <roberrudi006@gmail.com>
2021-01-29 17:30:02 +0300

    permmission setup

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\AdminController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+
+
+Route::get('/', [StudentController::class, 'index']);
+Route::get('/admin', [AdminController::class, 'index']);
