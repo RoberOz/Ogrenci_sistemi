790a3f279cbbe13603d87c57c6ca98ed17bf906e
RoberOZ <roberrudi006@gmail.com>
2021-01-30 19:51:56 +0300

    admin panel

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,13 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class AdminController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
+      $users = User::all();
 
-      return view('admin');
+      return view('admin')->with(compact('users'));
 
     }
 }
--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -3,19 +3,34 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Öğrenciler') }}</div>
+                  <div class="card-body">
+                      @if (session('status'))
+                          <div class="alert alert-success" role="alert">
+                              {{ session('status') }}
+                          </div>
+                      @endif
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                      <table width=100%>
+                        <tr height=35>
+                          <td align="center"><strong>-Öğrenci İsmi-</strong></td>
+                          <td align="center"><strong>-Öğrenci E-mail Adresi-</strong></td>
+                          <td align="center"><strong></strong></td>
+                          <td align="center"><strong></strong></td>
+                        </tr>
+                        @foreach ($users as $user)
+                          <tr height=35>
+                            <td align="center">{{$user->name}}</td>
+                            <td align="center">{{$user->email}}</td>
+                            <td align="center">Düzenle</td>
+                            <td align="center">Sil</td>
+                          </tr>
+                        @endforeach
+                      </table>
 
-                    {{ __('Admin Paneli') }}
-                </div>
+                  </div>
             </div>
         </div>
     </div>
